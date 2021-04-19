cb663b27b01c59929d202d294a78cfb61ff7ea45
lnarolski <narolskilukasz+github@gmail.com>
2021-04-19 14:47:03 +0200

    Another commit

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 #include <pty.h>
 #include <vector>
 #include <csignal>
+#include <fstream>
 
 enum RuntimeErrors
 {
     OPENPTY_CANT_CREATE_INTERFACE = 1,
     TOO_FEW_ARGS = 2,
+    CANT_CREATE_TTYCLONER_TMP_FILE = 3,
 };
 
 using namespace std;
@@ -59,7 +61,14 @@ for 2 OR 5 interface clones.");
     int status = 0;
     vector<int> masterDev, slaveDev;
 
-    for (int i = 0; i < numOfTtyIntrefaces; i++)
+    ofstream clonedInterfacesFile("/tmp/ttyCloner.txt");
+    if (!clonedInterfacesFile.good())
+    {
+        close(ttyDevice);
+        exit(CANT_CREATE_TTYCLONER_TMP_FILE);
+    }
+
+    for (int i = 0; !stopApplication && i < numOfTtyIntrefaces; i++)
     {
         int master, slave;
         status = openpty(&master, &slave, NULL, NULL, NULL);
@@ -81,8 +90,11 @@ for 2 OR 5 interface clones.");
         masterDev.push_back(master);
         slaveDev.push_back(slave);
 
-        printf("Slave%d name: %s\n", i, ttyname(slaveDev[slaveDev.size() - 1]));
+        //printf("Slave%d name: %s\n", i, ttyname(slaveDev[slaveDev.size() - 1]));
+        clonedInterfacesFile << ttyname(slaveDev[slaveDev.size() - 1]) << std::endl;
     }
+
+    clonedInterfacesFile.close();
 
     while (!stopApplication)
     {
@@ -94,8 +106,8 @@ for 2 OR 5 interface clones.");
         /// <returns></returns>
         if (receivedBytes > 0)
         {
-            putchar(buffer);
-            fflush(stdout);
+            /*putchar(buffer);
+            fflush(stdout);*/
 
             for (size_t i = 0; i < masterDev.size(); i++)
             {
@@ -113,7 +125,7 @@ for 2 OR 5 interface clones.");
             char slaveBuffer[4095];
             size_t j = 0;
             receivedBytes = read(masterDev[i], &buffer, 1);
-            while (receivedBytes > 0)
+            while (receivedBytes > 0 && j < 4095)
             {
                 slaveBuffer[j] = buffer;
                 ++j;
@@ -123,11 +135,11 @@ for 2 OR 5 interface clones.");
 
             if (j > 0)
             {
-                for (size_t k = 0; k < j; k++)
+                /*for (size_t k = 0; k < j; k++)
                 {
                     putchar(slaveBuffer[k]);
                 }
-                fflush(stdout);
+                fflush(stdout);*/
 
                 write(ttyDevice, &slaveBuffer, j);
             }
