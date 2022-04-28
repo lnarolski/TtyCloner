419c95ba48c8af0dd177a212aad856884e6a1873
lnarolski <narolskilukasz+github@gmail.com>
2022-04-28 15:54:36 +0200

    Changed parsing method for console arguments

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 #include <csignal>
 #include <fstream>
 #include "./cxxopts.hpp"
+#include <exception>
+#include <iostream>
 
 enum RuntimeErrors
 {
 	OPENPTY_CANT_CREATE_INTERFACE = 1,
-	TOO_FEW_ARGS = 2,
+	CANT_PARSE_ARGS = 2,
 	CANT_CREATE_TTYCLONER_TMP_FILE = 3,
+	UNSUPPORTED_BAUDRATE = 4,
+	REMOVE_TMP_FILE_ERROR = 5,
 };
 
 bool stopApplication = false;
@@ -22,137 +26,147 @@ void SignalHandler(int signal)
 	stopApplication = true;
 }
 
-cxxopts::Options options("TtyCloner", "Clone tty device to few pseudo ttys without worrying about blocking cloned device");
-
 int main(int argc, char* argv[])
 {
+	cxxopts::Options consoleOptions("TtyCloner", "Clone tty device to few pseudo ttys without worrying about blocking cloned device");
+	consoleOptions.allow_unrecognised_options();
+	consoleOptions.add_options()
+		("b,baudrate", "Set baudrate", cxxopts::value<int>() -> default_value("9600"))
+		("n,number", "Number of new pty interfaces", cxxopts::value<int>())
+		("i,interface", "Tty interface to clone", cxxopts::value<std::string>())
+		("f,file", "Create file /tmp/ttyCloner.txt with list of created pty interfaces")
+		("h,help", "Show this help")
+	;
+
+	cxxopts::ParseResult result;
+	try
+	{
+		result = consoleOptions.parse(argc, argv);
+	}
+	catch (const std::exception&)
+	{
+		return CANT_PARSE_ARGS;
+	}
+
+	if (result.count("help") || !result.count("number") || !result.count("interface"))
+	{
+		std::cout << consoleOptions.help() << std::endl;
+		exit(0);
+	}
+
 	std::signal(SIGABRT, SignalHandler);
 	std::signal(SIGTERM, SignalHandler);
 	std::signal(SIGKILL, SignalHandler);
 	std::signal(SIGSEGV, SignalHandler);
 
 	int numOfTtyIntrefaces;
-	char* clonedTtyInterface;
-	unsigned int baudrate = B9600;
+	const char* clonedTtyInterface;
+	unsigned int baudrate;
 	useconds_t sleepTime_ms = 200;
 
-	if (argc < 3)
-	{
-		printf("You have to set up first startup argument to number of tty interface clones and the second one with cloned Tty intreface and the third one with baudrate (default 9600 Bd), e.g.:\n\
-./TtyCloner 2 /dev/ttyS0 19200\n\
-OR\n\
-./TtyCloner 5 /dev/ttyUSB0\n\
-for 2 '/dev/ttyS0' with 19200 Bd baudrate OR 5 '/dev/ttyUSB0' with 9600 Bd baudrate interface clones.");
-		exit(TOO_FEW_ARGS);
-	}
-	else
-	{
-		numOfTtyIntrefaces = atol(argv[1]);
-		clonedTtyInterface = argv[2];
-	}
+	numOfTtyIntrefaces = result["number"].as<int>();
+	clonedTtyInterface = result["interface"].as<std::string>().c_str();
 
-	if (argc == 4)
+	unsigned int argBaudrate = atol(argv[3]);
+	sleepTime_ms = (useconds_t)((((double)19200.0) / argBaudrate) * 100);
+	switch (argBaudrate)
 	{
-		unsigned int argBaudrate = atol(argv[3]);
-		sleepTime_ms = (useconds_t)((((double)19200.0) / argBaudrate) * 100);
-		switch (argBaudrate)
-		{
-		case 0:
-			baudrate = B0;
-			break;
-		case 50:
-			baudrate = B50;
-			break;
-		case 75:
-			baudrate = B75;
-			break;
-		case 110:
-			baudrate = B110;
-			break;
-		case 134:
-			baudrate = B134;
-			break;
-		case 150:
-			baudrate = B150;
-			break;
-		case 200:
-			baudrate = B200;
-			break;
-		case 300:
-			baudrate = B300;
-			break;
-		case 600:
-			baudrate = B600;
-			break;
-		case 1200:
-			baudrate = B1200;
-			break;
-		case 1800:
-			baudrate = B1800;
-			break;
-		case 2400:
-			baudrate = B2400;
-			break;
-		case 4800:
-			baudrate = B4800;
-			break;
-		case 9600:
-			baudrate = B9600;
-			break;
-		case 19200:
-			baudrate = B19200;
-			break;
-		case 38400:
-			baudrate = B38400;
-			break;
-		case 57600:
-			baudrate = B57600;
-			break;
-		case 115200:
-			baudrate = B115200;
-			break;
-		case 230400:
-			baudrate = B230400;
-			break;
-		case 460800:
-			baudrate = B460800;
-			break;
-		case 500000:
-			baudrate = B500000;
-			break;
-		case 576000:
-			baudrate = B576000;
-			break;
-		case 921600:
-			baudrate = B921600;
-			break;
-		case 1000000:
-			baudrate = B1000000;
-			break;
-		case 1152000:
-			baudrate = B1152000;
-			break;
-		case 1500000:
-			baudrate = B1500000;
-			break;
-		case 2000000:
-			baudrate = B2000000;
-			break;
-		case 2500000:
-			baudrate = B2500000;
-			break;
-		case 3000000:
-			baudrate = B3000000;
-			break;
-		case 3500000:
-			baudrate = B3500000;
-			break;
-		case 4000000:
-			baudrate = B4000000;
-			break;
-		default:
-			break;
-		}
+	case 0:
+		baudrate = B0;
+		break;
+	case 50:
+		baudrate = B50;
+		break;
+	case 75:
+		baudrate = B75;
+		break;
+	case 110:
+		baudrate = B110;
+		break;
+	case 134:
+		baudrate = B134;
+		break;
+	case 150:
+		baudrate = B150;
+		break;
+	case 200:
+		baudrate = B200;
+		break;
+	case 300:
+		baudrate = B300;
+		break;
+	case 600:
+		baudrate = B600;
+		break;
+	case 1200:
+		baudrate = B1200;
+		break;
+	case 1800:
+		baudrate = B1800;
+		break;
+	case 2400:
+		baudrate = B2400;
+		break;
+	case 4800:
+		baudrate = B4800;
+		break;
+	case 9600:
+		baudrate = B9600;
+		break;
+	case 19200:
+		baudrate = B19200;
+		break;
+	case 38400:
+		baudrate = B38400;
+		break;
+	case 57600:
+		baudrate = B57600;
+		break;
+	case 115200:
+		baudrate = B115200;
+		break;
+	case 230400:
+		baudrate = B230400;
+		break;
+	case 460800:
+		baudrate = B460800;
+		break;
+	case 500000:
+		baudrate = B500000;
+		break;
+	case 576000:
+		baudrate = B576000;
+		break;
+	case 921600:
+		baudrate = B921600;
+		break;
+	case 1000000:
+		baudrate = B1000000;
+		break;
+	case 1152000:
+		baudrate = B1152000;
+		break;
+	case 1500000:
+		baudrate = B1500000;
+		break;
+	case 2000000:
+		baudrate = B2000000;
+		break;
+	case 2500000:
+		baudrate = B2500000;
+		break;
+	case 3000000:
+		baudrate = B3000000;
+		break;
+	case 3500000:
+		baudrate = B3500000;
+		break;
+	case 4000000:
+		baudrate = B4000000;
+		break;
+	default:
+		return UNSUPPORTED_BAUDRATE;
+		break;
 	}
 
 	char buffer[4095];
@@ -170,11 +184,16 @@ for 2 '/dev/ttyS0' with 19200 Bd baudrate OR 5 '/dev/ttyUSB0' with 9600 Bd baudr
 	int status = 0;
 	std::vector<int> masterDev, slaveDev;
 
-	std::ofstream clonedInterfacesFile("/tmp/ttyCloner.txt");
-	if (!clonedInterfacesFile.good())
+	std::ofstream clonedInterfacesFile;
+	if (result.count("file"))
 	{
-		close(ttyDevice);
-		exit(CANT_CREATE_TTYCLONER_TMP_FILE);
+		clonedInterfacesFile.open("/tmp/ttyCloner.txt");
+
+		if (!clonedInterfacesFile.good())
+		{
+			close(ttyDevice);
+			exit(CANT_CREATE_TTYCLONER_TMP_FILE);
+		}
 	}
 
 	for (int i = 0; !stopApplication && i < numOfTtyIntrefaces; i++)
@@ -189,8 +208,11 @@ for 2 '/dev/ttyS0' with 19200 Bd baudrate OR 5 '/dev/ttyUSB0' with 9600 Bd baudr
 				close(slaveDev[i]);
 			}
 
-			clonedInterfacesFile.close();
-			std::remove("/tmp/ttyCloner.txt");
+			if (result.count("file"))
+			{
+				clonedInterfacesFile.close();
+				std::remove("/tmp/ttyCloner.txt");
+			}
 
 			perror("Openpty can't create interface: ");
 			exit(OPENPTY_CANT_CREATE_INTERFACE);
@@ -203,10 +225,12 @@ for 2 '/dev/ttyS0' with 19200 Bd baudrate OR 5 '/dev/ttyUSB0' with 9600 Bd baudr
 		slaveDev.push_back(slave);
 
 		//printf("Slave%d name: %s\n", i, ttyname(slaveDev[slaveDev.size() - 1]));
-		clonedInterfacesFile << ttyname(slaveDev[slaveDev.size() - 1]) << std::endl;
-	}
 
-	clonedInterfacesFile.close();
+		if (result.count("file"))
+			clonedInterfacesFile << ttyname(slaveDev[slaveDev.size() - 1]) << std::endl;
+	}
+	if (result.count("file"))
+		clonedInterfacesFile.close();
 
 	while (!stopApplication)
 	{
@@ -243,7 +267,9 @@ for 2 '/dev/ttyS0' with 19200 Bd baudrate OR 5 '/dev/ttyUSB0' with 9600 Bd baudr
 
 	close(ttyDevice);
 
-	std::remove("/tmp/ttyCloner.txt");
+	if (result.count("file"))
+		if (std::remove("/tmp/ttyCloner.txt"));
+			return REMOVE_TMP_FILE_ERROR;
 
 	return 0;
 }
